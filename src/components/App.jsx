9c56d4410635e6b4e31da926e6e578e0c5edecd4
rrobinson18 <robinson.rakesha@gmail.com>
2019-03-18 00:55:34 -0400

    second phase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './Card';
+import People from './People';
+import Logo from './logo.png';
 
 
 
-class App extends React.Component {
+class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            films: []
-        };
+            films: [],
+            people: [],
+            loadFilms: false,
+            loadPeople: false
+        }
     }
 
     componentDidMount() {
         fetch('https://ghibliapi.herokuapp.com/films')
-        .then(res => res.json())
-        .then(films => this.setState({ films }))
+            .then(res => res.json())
+            .then(films => {
+                this.setState({ films })
+            })
+
+        fetch('https://ghibliapi.herokuapp.com/people')
+            .then(peop => peop.json())
+            .then(people => {
+                this.setState({ people })
+            })
+    }
+
+
+    handleFilms() {
+        this.setState({
+            loadFilms: true
+        })
+    }
+
+    handlePeople() {
+        this.setState({
+            loadPeople: true
+        })
     }
 
 
     render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    {this.state.films.map(films => <Card key={films.id} films={films} />)}
+        if (this.state.loadFilms === false && this.state.loadPeople === false) {
+            return (
+<>
+                <div className="row p-5">
+                    <img src={Logo} alt="" />
                 </div>
-            </div>
-        );
+                <div className="row p-3">
+                
+                    <button className="btn btn-outline-info" onClick={(e => this.handleFilms(e))}>Load Films</button>
+                </div>
+                <div className="row p-3">
+                    <button className="btn btn-outline-info" onClick={(e => this.handlePeople(e))}>Load People</button>
+                </div>
+                </>
+        )
+        }
+
     }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Card from './components/Card';
+import People from './components/People';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
